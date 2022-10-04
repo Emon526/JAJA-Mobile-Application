d6af5be0dce07862802162f1c0b927423774ac36
Emon526 <emonats526@gmail.com>
2022-10-04 19:43:24 +0600

    upload image

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -5,9 +5,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-
 import 'package:image_picker/image_picker.dart';
-
 import '../models/usermodel.dart';
 import '../services/authservice.dart';
 
